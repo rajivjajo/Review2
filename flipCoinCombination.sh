5af56019cd2e4161f9fbc6106664239bc8224148
rajivjajo <rajivjajodia82552@gmail.com>
2022-12-02 20:58:21 +0530

    singlet combo

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
 
-flipCoin=$(( RANDOM%2 ));
+#variables
+declare -A coin
+
+head=0;
+tail=0;
+i=1;
 
 
-if [[ $flipCoin -eq 0 ]]
-then
-	
+while (( tail<44 && head<44))
+do
+	flip=$((RANDOM%2));
+	coin[$i]=$flip;
+		((i++));
+	case $flip in
+		1)
+			((head++))
+		;;
+		*)
+			((tail++))
+		;;
+	esac
 
-		echo "Head out !";
-else
-		echo "Tail out !";
+done
+		
 
-fi
+echo "Singlet combination ${coin[@]}";	
+
+percentage_head=$(($head/21));
+
+echo "percentage of singlet  combo is $percentage_head";
+
+percentage_tail=$(($tail/21));
+
+echo "percentage of singlet  combo is $percentage_tail";
